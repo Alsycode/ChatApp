a0c8b87b716efd7ecf21b2505b46357e4f0b695c
Alsycode <120390865+Alsycode@users.noreply.github.com>
2025-03-03 15:11:55 +0530

    03-03-2025-final

--- backend/frontend/src/components/Login.jsx
+++ backend/frontend/src/components/Login.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useForm } from "react-hook-form"
 import axios from 'axios';
-import { useAuth } from '../context/authProvider';
+import { useAuth } from '../context/Authprovider';
 import { Link } from "react-router-dom";
 function Login() {
     const {
